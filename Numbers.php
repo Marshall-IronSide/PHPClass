e2be1edcfb64c9264154564dea2603ff132cae21
Marshall-IronSide <meliodas.martin@gmail.com>
2025-03-19 20:07:00 +0000

    Last php class

--- Numbers.php
+++ Numbers.php
@@ -40,4 +40,15 @@ var_dump(is_numeric($a));
 var_dump(is_numeric($b));
 var_dump(is_numeric($c));
 var_dump(is_numeric($d));
+// Cast float to int
+echo"<br/>";
+$x = 23465.768;
+$int_cast = (int)$x;
+echo $int_cast;
+
+echo"<br/>";
+// cast string to int
+$x = "23465.768";
+$y = (int)$x; 
+echo $y;
 ?>

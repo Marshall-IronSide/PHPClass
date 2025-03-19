d0163c63b0da40a6d20a4c2c3c45f04d00f27038
Marshall-IronSide <meliodas.martin@gmail.com>
2025-03-19 20:39:20 +0000

    Last php class

--- Numbers.php
+++ Numbers.php
@@ -86,4 +86,42 @@ var_dump($b);
 var_dump($c);
 var_dump($d);
 var_dump($e);
+
+//Converting Objects into Arrays
+class car {
+    public $color;
+    public $model;
+    public function __construct($color, $model) {
+        $this->color = $color;
+        $this->model = $model;
+}
+public function __message() {
+return "My car is a".$this ->color ." ". $this->model ."!";
+}
+}
+$myCar = new car("red","volvo");
+$myCar = (array) $myCar;
+echo "<br/>";
+var_dump($myCar);
+
+//cast to object
+$a = 5;
+$b = 5.34;
+$c = "hello";
+$d = true;
+$e = NULL;
+
+$a = (object) $a;
+$b = (object) $b;
+$c = (object) $c;
+$d = (object) $d;
+$e = (object) $e;
+echo"<br/>";
+var_dump($a);
+var_dump($b);
+var_dump($c);
+var_dump($d);
+var_dump($e);
+
+//Associative arrays converts into objects with the keys as property names and values as property values
 ?>

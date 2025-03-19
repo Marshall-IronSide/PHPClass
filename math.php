eaaf94adb15bdd3970a8ec51795eb0c8d4ac0e78
Marshall-IronSide <meliodas.martin@gmail.com>
2025-03-19 21:14:21 +0000

    Last php class

---
+++ math.php
@@ -0,0 +1,25 @@
+<?php
+//pi() Function
+echo(pi());
+//min() and max() Functions
+echo("<br/>");
+echo(min(0, 150, 30, 20, -8, -200));
+echo("<br/>");
+echo(max(0, 150, 30, 20, -8, -200));
+//return the absolute (positive) value of a number
+echo("<br/>");
+echo(abs(-6.7));
+//the square root of a number
+echo("<br/>");
+echo(sqrt(64));
+//round a floating-point number to its nearest integer
+echo("<br/>");
+echo(round(0.49));
+echo("<br/>");
+echo(round(0.69));
+//generate a random number
+echo("<br/>");
+echo(rand());
+echo("<br/>");
+echo(rand(1,10));
+?>
